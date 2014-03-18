0d5e264426772e2935106a666bf6f5ac430c904b
trmchale1 <trmchale@gmail.com>
2014-03-18 07:43:37 -0400

    first commit

--- iOS-HelloMe-MapKit/HMViewController.h
+++ iOS-HelloMe-MapKit/HMViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+@interface HMViewController : UIViewController <MKMapViewDelegate>
 
-@interface HMViewController : UIViewController
+@property (nonatomic, strong) IBOutlet MKMapView *mapView;
+
 
 @end
--- iOS-HelloMe-MapKit/HMViewController.m
+++ iOS-HelloMe-MapKit/HMViewController.m
@@ -13,11 +13,24 @@
 @end
 
 @implementation HMViewController
+@synthesize mapView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.mapView.delegate = self;}
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
+    
+    MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
+    point.coordinate = userLocation.coordinate;
+    point.title = @"Where am I?";
+    point.subtitle = @"I'm here!!!";
+    
+    [self.mapView addAnnotation:point];
 }
 
 - (void)didReceiveMemoryWarning
