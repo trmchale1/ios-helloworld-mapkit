d92e4e51b254f0eace970a487780a7e0da624638
trmchale1 <trmchale@gmail.com>
2014-03-19 03:28:58 -0400

    Added some annotations

---
+++ iOS-HelloMe-MapKit/myAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  myAnnotation.h
+//  iOS-HelloMe-MapKit
+//
+//  Created by Tim McHale on 3/19/14.
+//  Copyright (c) 2014 Tim McHale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface myAnnotation : NSObject
+
+@end
---
+++ iOS-HelloMe-MapKit/myAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  myAnnotation.m
+//  iOS-HelloMe-MapKit
+//
+//  Created by Tim McHale on 3/19/14.
+//  Copyright (c) 2014 Tim McHale. All rights reserved.
+//
+
+#import "myAnnotation.h"
+
+@implementation myAnnotation
+
+@end
